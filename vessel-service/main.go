bd45775af587e7e4e56de9fc261a46e0f87215f1
Marcelo Aguero <marceloaguero@gmail.com>
2017-12-11 10:49:30 -0300

    Add vessel service

---
+++ vessel-service/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"log"
+	"net"
+
+	"google.golang.org/grpc/reflection"
+
+	pb "github.com/marceloaguero/shipper/vessel-service/proto/vessel"
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":50051"
+)
+
+type Repository interface {
+	FindAvailable(*pb.Specification) (*pb.Vessel, error)
+}
+
+type VesselRepository struct {
+	vessels []*pb.Vessel
+}
+
+// FindAvailable - checks a specification against a map of vessels,
+// if capacity and max weight are below a vessels capacity and max weight,
+// then return that vessel.
+func (repo *VesselRepository) FindAvailable(spec *pb.Specification) (*pb.Vessel, error) {
+	for _, vessel := range repo.vessels {
+		if spec.Capacity <= vessel.Capacity && spec.MaxWeight <= vessel.MaxWeight {
+			return vessel, nil
+		}
+	}
+	return nil, errors.New("No vessel found by that spec")
+}
+
+// Our grpc service handler
+type service struct {
+	repo Repository
+}
+
+func (s *service) FindAvailable(ctx context.Context, req *pb.Specification) (*pb.Response, error) {
+
+	// Find the next available vessel
+	vessel, err := s.repo.FindAvailable(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.Response{Vessel: vessel}, nil
+}
+
+func main() {
+	vessels := []*pb.Vessel{
+		&pb.Vessel{Id: "vessel001", Name: "Kane's Salty Secret", MaxWeight: 200000, Capacity: 500},
+	}
+	repo := &VesselRepository{vessels}
+
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+
+	pb.RegisterVesselServiceServer(s, &service{repo})
+
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+	/*	srv := micro.NewService(
+			micro.Name("go.micro.srv.vessel"),
+			micro.Version("latest"),
+		)
+
+		srv.Init()
+
+		// Register our implementation with
+		pb.RegisterVesselServiceHandler(srv.Server(), &service{repo})
+
+		if err := srv.Run(); err != nil {
+			fmt.Println(err)
+		}	*/
+}
